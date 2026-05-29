91fdb5a0d7941efe22fa8767e43881c36b8054c5
Arthemeas <stormchicken1313@gmail.com>
2026-05-29 02:01:09 -0700

    fixed ActiveAgent model constructors to allign with the other models

--- src/models/RepairBot.java
+++ src/models/RepairBot.java
@@ -8,12 +8,13 @@ import java.awt.*;
 public class RepairBot extends ActiveAgent {
 
     private static final Color BASE_COLOR = new Color(170, 221, 255);
+    private static final Color DEAD_COLOR = new Color(35, 45, 55);
 
     private int repairPower;
     private NetworkNode currentTarget;
 
-    public RepairBot(int row, int col, int repairPower) {
-        super(row, col, 100, 5, 1);
+    public RepairBot(int row, int col, int repairPower, int scanRange) {
+        super(row, col, 100, 5, scanRange);
         this.repairPower = repairPower;
         this.currentTarget = null;
     }
@@ -23,12 +24,12 @@ public class RepairBot extends ActiveAgent {
 
     @Override
     public Color getColor() {
-        return BASE_COLOR;
+        return getHealthColor(BASE_COLOR, DEAD_COLOR);
     }
 
     @Override
     public String getTypeName() {
-        return "models.RepairBot";
+        return "Repair Bot";
     }
 
     @Override
